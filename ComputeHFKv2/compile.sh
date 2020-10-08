dcb3baa4db0c1b879e05c06f0d3559b5388ce129
Nathan Dunfield <nathan@dunfield.info>
2020-10-08 01:31:31 +0000

    Cleanup of ComputeHFKv2 directory

--- ComputeHFKv2/compile.sh
+++ ComputeHFKv2/compile.sh
@@ -1,4 +1,4 @@
-g++ -std=c++11 -o ../python_src/zs_hfk_binary -O3 Main.cpp Utility.cpp Min.cpp Max.cpp Crossing.cpp Simplify.cpp HomologyRank.cpp Report.cpp Diagrams.cpp Alternating.cpp KnotFloer.cpp
+g++ -std=c++11 -O3 Main.cpp Utility.cpp Min.cpp Max.cpp Crossing.cpp Simplify.cpp HomologyRank.cpp Report.cpp Diagrams.cpp Alternating.cpp KnotFloer.cpp
 
 
 
