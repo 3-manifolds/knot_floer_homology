f7329bfa509ff1a90ad8aca5b677eb86c2e71957
Nathan Dunfield <nathan@dunfield.info>
2021-02-13 13:50:41 -0600

    Made sdist tarball work

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Command, Extension
-from Cython.Build import cythonize
 import re, sys, os, shutil, glob, subprocess
+
 
 # Get version number from module
 version = re.search("__version__ = '(.*)'",
@@ -8,7 +8,9 @@ version = re.search("__version__ = '(.*)'",
 
 cpp_dir = 'ComputeHFKv2'
 cpp_sources = glob.glob(cpp_dir + os.sep + '*.cpp')
-cpp_sources.remove(cpp_dir + os.sep + 'Main.cpp')
+unused_main_cpp = cpp_dir + os.sep + 'Main.cpp'
+if unused_main_cpp in cpp_sources:
+    cpp_sources.remove(unused_main_cpp)
 
 if sys.platform.startswith('win'):
     extra_compile_args = ['/Ox']
@@ -38,10 +40,14 @@ class HFKClean(Command):
             if os.path.exists(file):
                 os.remove(file)
 
-if 'clean' not in sys.argv:
-    file = 'cython_src/hfk.pyx'
-    if os.path.exists(file):
-        cythonize([file])
+try:
+    from Cython.Build import cythonize
+    if 'clean' not in sys.argv and cythonize is not None:
+        file = 'cython_src/hfk.pyx'
+        if os.path.exists(file):
+            cythonize([file])
+except ImportError:
+    pass
 
 setup(
     name='knot_floer_homology',
