8154104c496b2224e06a83f9075adc0d369d5517
Nathan Dunfield <nathan@dunfield.info>
2022-10-29 18:57:38 -0500

    For debugging, print the knot whose regression test fails

--- python_src/test.py
+++ python_src/test.py
@@ -17,13 +17,16 @@ def matches_saved_HFK_results():
     >>> matches_saved_HFK_results()
     True
     """
+    failures = 0
     for datum in regression_data():
         name = datum.pop('name')
         pd = datum.pop('PD_code')
         result = hfk.pd_to_hfk(repr(pd))
         if datum != result:
-            return False
-    return True
+            print('Regression failure: ' + name)
+            failures += 1
+                
+    return failures == 0
 
 if __name__ == '__main__':
     import doctest
